063e27c12d6196040ee1d75ac32a529383413fe1
Eloy Toro <eloytoro@gmail.com>
2018-07-11 14:11:54 +0200

    remove memoization

--- index.js
+++ index.js
@@ -12,24 +12,12 @@ const update = (source, key, value) => Object.assign(
 
 const toarr = (val) => [].slice.call(val);
 
-const once = (fn) => {
-  let called = false;
-  let memoized;
-  return function () {
-    if (!called) {
-      called = true;
-      memoized = fn.apply(this, arguments);
-    }
-    return memoized;
-  }
-};
-
 const echelon = (wrap, unwrap, traverse) => {
   const ctx = Object.assign(
     map(wrap, method => function Wrapper() {
-      return echelon.call(ctx, wrap, unwrap, once(() =>
+      return echelon.call(ctx, wrap, unwrap, () =>
         method.apply(ctx, [traverse.call(ctx)].concat(toarr(arguments)))
-      ));
+      );
     }),
     map(unwrap, unwrap => function Unwrapper() {
       return unwrap.apply(ctx, [traverse.call(ctx)].concat(toarr(arguments)));
